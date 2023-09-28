24e8556f5f5112aeca1862da98e76ea2f05e13c0
Abdalrahman-Muhammad <abdelrhmanmohamed421@gmail.com>
2023-09-28 10:57:07 +0300

    select fields in search control

--- src/controllers/search.js
+++ src/controllers/search.js
@@ -7,17 +7,19 @@ import ErrorAPI from '../utils/errorAPI.js';
 export let handleSearch = asyncHandler(async (req, res, next) => {
   const searchQuery = req.query.search;
   const regexSearch = new RegExp(searchQuery, 'i');
-  const users = await UserModel.find({ full_name: regexSearch, role: 'Company' });
-  const services = await Service.find({ title: regexSearch });
-  if (!users && !services) {
+  const companies = await UserModel.find({ full_name: regexSearch, role: 'Company' }).select({
+    full_name: 1,
+  });
+  const services = await Service.find({ title: regexSearch }).select({ title: 1 });
+  if (!companies && !services) {
     return next(new ErrorAPI('لا يوجد مستخدم', 404));
   }
 
   res.json({
     status: 'success',
     data: {
-      users: users,
-      services: services,
+      companies,
+      services,
     },
   });
 });
